e72ec7f29631288d00555970303c25e281ec687b
최영찬 <gks54410@gmail.com>
2022-05-11 19:41:28 +0900

    Feat: EurekaClient 활성화 및 ConfigurationPropertiesScan 추가

--- src/main/java/propofol/alarmservice/AlarmServiceApplication.java
+++ src/main/java/propofol/alarmservice/AlarmServiceApplication.java
@@ -2,8 +2,12 @@ package propofol.alarmservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@ConfigurationPropertiesScan(basePackages = "propofol.alarmservice.api.common.properties")
 public class AlarmServiceApplication {
 
 	public static void main(String[] args) {
